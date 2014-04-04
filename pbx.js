5d249c79b7ff4372a6f7320370cd41926ecab25a
dstrek <batman@dstrek.com>
2014-04-04 16:03:30 +1100

    sync

--- pbx.js
+++ pbx.js
@@ -1,14 +1,40 @@
+var pbx_main = '';
+
 function inject(source, dest, data) {
 	// grab the template source by id
-	// compile it 
+	// compile it and return html with context
 	// insert into html of the dest id
-	var tpl = $('#'+source).html();
-	var compiled = Handlebars.compile(tpl);
-	$('#'+dest).html(compiled);
+	var src = $('#'+source).html();
+	var template = Handlebars.compile(src || '');
+	var html = template(data || {});
+	$('#'+dest).html(html);
 	console.log('injected', source, dest);
 }
 
+function render_menu() {
+	// grab the datas for active state and badge counts
+	var data = {};
+	data.active = pbx_main;
+	data.users = 12;
+	data.lines = 8;
+	data.groups = 1;
+
+	inject('t_menu', 'menu', data);	
+}
+
+function render_main() {
+	// probably have to get data and shit
+	inject('t_'+pbx_main, 'main');
+}
+
+function set_main(main) {
+	pbx_main = main;
+
+	render_menu();
+	render_main();
+}
+
 $(document).ready(function() {
-	inject('t_menu', 'menu');	
+	set_main('users');
 });
 
